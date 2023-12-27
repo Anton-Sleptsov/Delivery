85721a3474c65cb3c914ce4525b3e4a771efa490
Anton-Sleptsov <arslepcov@kait20.ru>
2023-12-27 20:09:14 +0300

    Добавлен проект vue

---
+++ Delivery-vue/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+  import Modal from './components/Modal.vue';
+  import { ref } from 'vue';
+
+  const isOpen = ref(false)
+</script>
+
+<template>
+  <div class="page-wraper">
+    <Header @togglModal="isOpen = !isOpen"/>
+    <main class="main">
+      <router-view></router-view>
+    </main>
+    <Modal :isOpen = 'isOpen' @togglModal="isOpen = !isOpen"/>
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
